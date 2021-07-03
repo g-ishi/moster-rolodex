73f39a3a8dcd125ba67fa11d985855e6ae787381
石井源一 <ishiigenichi@ishiigenichinoMacBook-Air.local>
2021-07-03 21:50:05 +0900

    change files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list';
+import { SearchBox } from './components/search-box/search-box';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+    }
+
+    // bindを使う場合の例
+    // アロー関数を使う場合にはthisコンテキストのバインドは不要
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange = e => {
+    // 
+    this.setState({ searchField: e.target.value });
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filterdMonsters = monsters.filter(monsters => monsters.name.includes(searchField))
+    return (
+      <div className="App">
+        <h1> monster rolodex </h1>
+        <SearchBox
+          placeholder="search text..."
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filterdMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
